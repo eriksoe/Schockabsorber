34131e2f884d3cd0203b2a00ffd90cdab9e96383
Erik Søe Sørensen <ess@trifork.com>
2013-06-12 18:43:58 +0200

    Loader: improve RLE decoding performance (~4x)

--- shockabsorber/loader/rle.py
+++ shockabsorber/loader/rle.py
@@ -1,24 +1,25 @@
 import pyglet.image
 
-
 # Returns an ImageData
 def rle_decode(width, height, fullwidth, bpp, encoded_data):
-    res = "" # TODO: pre-allocated bytestring
+    bytes_to_output = fullwidth*height
+    res = bytearray(bytes_to_output)
     in_pos = 0; in_len = len(encoded_data)
     out_pos = 0
     #x = 0; y = 0
     #fullwidth = width*(bpp//8)
-    bytes_to_output = fullwidth*height
     while in_pos < in_len and out_pos < bytes_to_output:
         d = ord(encoded_data[in_pos]); in_pos+=1
         if d >= 128:
             run_length = 257-d
             v = encoded_data[in_pos]; in_pos+=1
-            for i in range(run_length): res += v
+            for i in range(run_length):
+                res[out_pos+i] = v
             out_pos += run_length
         else:
             lit_length = 1+d
-            res += encoded_data[in_pos:in_pos+lit_length]; in_pos += lit_length
+            res[out_pos:out_pos+lit_length] = encoded_data[in_pos:in_pos+lit_length]
+            in_pos += lit_length
             out_pos += lit_length
     print "DB| rle end: out_pos: %d vs. expected %d; in_pos: %d vs. %d" % (out_pos, bytes_to_output, in_pos, in_len)
     #print "DB| rle: converted %s to %s" % (encoded_data, res)
@@ -37,9 +38,10 @@ def make_greyscale_image(width, height, fullwidth, data):
     return pyglet.image.ImageData(width, height, 'I', data, -fullwidth)
 
 def make_8bit_rbg_image(width, height, fullwidth, data):
+    # TODO: This is the wrong palette.
     color_data = ""
     for c in data:
-        nr = ord(c)
+        nr = c
         r = nr >> 5
         g = (nr >> 2) & 7
         b = nr & 3
@@ -52,35 +54,45 @@ def make_8bit_rbg_image(width, height, fullwidth, data):
         color_data += chr(b*(255//3))
     return pyglet.image.ImageData(width, height, 'RGB', color_data, -3*fullwidth)
 
+scale_table_31_to_255 = map(lambda v: (v*255)//31 , range(32))
+
 def make_16bit_rbg_image(width, height, fullwidth, data):
-    color_res = ""
+    color_res = bytearray(3*width*height)
+    pos = 0
     for y in range(height):
+        row_start = y*fullwidth
         for x in range(width):
-            highbits = ord(data[y*fullwidth + x])
-            lowbits  = ord(data[y*fullwidth + x + width])
+            pix_start = row_start + x
+            highbits = data[pix_start]
+            lowbits  = data[pix_start + width]
             bits = (highbits << 8) | lowbits
             a = bits >> 15
             r = (bits >> 10) & 31
             g = (bits >> 5) & 31
             b = bits & 31
             if a>0: r=31; g=31; b=0
-            color_res += chr(r*(255//31))
-            color_res += chr(g*(255//31))
-            color_res += chr(b*(255//31))
+            color_res[pos]   = scale_table_31_to_255[r]
+            color_res[pos+1] = scale_table_31_to_255[g]
+            color_res[pos+2] = scale_table_31_to_255[b]
+            pos += 3
+    color_res = str(color_res)
     return pyglet.image.ImageData(width, height, 'RGB', color_res, -3*width)
 
 def make_32bit_rbg_image(width, height, fullwidth, data):
-    color_res = ""
+    color_res = bytearray(4*width*height)
+    pos = 0
     for y in range(height):
         for x in range(width):
             r = data[y*fullwidth + x]
             g = data[y*fullwidth + x + width]
             b = data[y*fullwidth + x + 2*width]
             a = data[y*fullwidth + x + 3*width]
-            color_res += r
-            color_res += g
-            color_res += b
-            color_res += a
+            color_res[pos]   = r
+            color_res[pos+1] = g
+            color_res[pos+2] = b
+            color_res[pos+3] = a
+            pos += 4
+    color_res = str(color_res)
     return pyglet.image.ImageData(width, height, 'RGBA', color_res, -4*width)
 
 
